3803af9e3f0c81c2f152d7e7f6cd1a473ada6d02
andres <afballesterosl@gmail.com>
2022-11-01 20:33:55 -0500

    pausa backend inicio frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,20 @@
+const data = {
+    productos: [
+        {
+            "name": "Huevos",
+            "slug": "",
+            "price": "",
+            "countInSock": "",
+            "description": "",
+            "image": ""
+        },
+        {
+            "name": "Pollo",
+            "slug": "",
+            "price": "",
+            "countInSock": "",
+            "description": "",
+            "image": ""
+        },
+    ]
+};
