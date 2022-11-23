7c6976933670928fe3d2959b217c72ab9624c51e
andres <afballesterosl@gmail.com>
2022-11-22 21:52:46 -0500

    prueba

--- backend/src/routes/venta.js
+++ backend/src/routes/venta.js
@@ -13,7 +13,7 @@ var router = express.Router();
 router.post('/save', Venta.save);
 
 //Obtener todos los artículos sin archivar.
-router.get('/venta', Venta.getVenta);
+router.get('/ventsSa', Venta.getVenta);
 
 //Eliminar un artículo. Le pasamos el parámetro :id como obligatorio.
 router.delete('/delete/:id', Venta.delete);
